de05899f7f11c77189fee48f382688f16be34d07
Jonathan <jonathan.bruno907@gmail.com>
2026-05-13 15:36:46 -0300

    feat: migra cadastro para banco de dados
    Com essa atualização, ocorreu a substituição do armazenamento em dicionário para o uso de banco de dados com SQLite. A aplicação permite salvar os dados mesmo após o encerramento do programa, assim como também foram adicionados novos campos de cadastro. A integração com Tkinter e SQLite tornou o sistema mais organizado e próximo de aplicações reais.

--- python-exercicios/lista_cadastro.py
+++ python-exercicios/lista_cadastro.py
@@ -1,46 +1,52 @@
 import tkinter as tk
 from tkinter import messagebox
-dados = {}
-def adicionar():
-    nome = entry_nome.get()
-    idade = entry_idade.get()
-    if nome == ""or idade == "":
-        messagebox.showwarning("Aviso", "Preencha todos os campos!")
+import sqlite3
+conexao = sqlite3.connect("dados.db")
+cursor = conexao.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS pessoas(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    cpf TEXT,
+    renda REAL
+)
+""")
+conexao.commit()
+def cadastrar():
+    nome = entrada_nome.get()
+    cpf = entrada_cpf.get()
+    renda = entrada_renda.get()
+    if nome ==""or cpf ==""or renda =="":
+        messagebox.showwarning("Atenção", "Preencha todos os campos!")
         return
-
-    dados[nome] = idade
-    atualizar_lista()
-    entry_nome.delete(0, tk.END)
-    entry_idade.delete(0, tk.END)
-
-def atualizar_lista():
+    cursor.execute("INSERT INTO pessoas (nome, cpf, renda) VALUES (?, ?, ?)", (nome, cpf, float(renda)))
+    conexao.commit()
+    messagebox.showinfo("Sucesso", "Cadastro realizado!")
+    entrada_nome.delete(0, tk.END)
+    entrada_cpf.delete(0, tk.END)
+    entrada_renda.delete(0, tk.END)
+def listar():
     lista.delete(0, tk.END)
-    for nome, idade in dados.items():
-        lista.insert(tk.END, f"{nome} - {idade} anos")
-
-def remover():
-    selecionado = lista.curselection()
-    if not selecionado:
-        messagebox.showwarning("Aviso", "Selecione alguém para remover!")
-        return
-    texto = lista.get(selecionado)
-    nome = texto.split(" - ")[0]
-    del dados[nome]
-    atualizar_lista()
+    cursor.execute("SELECT nome, cpf, renda FROM pessoas")
+    dados = cursor.fetchall()
+    for pessoa in dados:
+        lista.insert(tk.END, f"Nome: {pessoa[0]} | CPF: {pessoa[1]} | renda: R$ {pessoa[2]:.2f}")
 
 janela = tk.Tk()
-janela.title("Cadastro com Dicionário")
-tk.Label(janela, text="Nome:").pack()
-entry_nome = tk.Entry(janela)
-entry_nome.pack()
-tk.Label(janela, text="Idade:").pack()
-entry_idade = tk.Entry(janela)
-entry_idade.pack()
-
-tk.Button(janela, text= "Adicionar", command=adicionar).pack(pady=5)
-tk.Button(janela, text="Remover", command=remover).pack(pady=5)
-
-lista = tk.Listbox(janela, width=40)
+janela.title("Cadastro de Pessoas")
+janela.geometry("500x400")
+tk.Label(janela, text="Nome").pack()
+entrada_nome = tk.Entry(janela)
+entrada_nome.pack()
+tk.Label(janela, text="CPF").pack()
+entrada_cpf = tk.Entry(janela)
+entrada_cpf.pack()
+tk.Label(janela, text="Renda").pack()
+entrada_renda = tk.Entry(janela)
+entrada_renda.pack()
+tk.Button(janela, text="Cadastrar", command=cadastrar).pack(pady=5)
+tk.Button(janela, text="Listar", command=listar).pack(pady=5)
+lista = tk.Listbox(janela, width=60)
 lista.pack(pady=10)
-
 janela.mainloop()
+conexao.close()
